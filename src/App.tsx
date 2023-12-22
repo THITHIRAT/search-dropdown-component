d42c4e4398e30b894c1cf48f39bc26b29d708155
Thithirat <thithirat.chsw@gmail.com>
2023-12-22 16:40:27 +0700

    refactor: clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { useState } from 'react'
 import SearchComponent from './component/SearchComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className='bg-gray-100 flex items-center justify-center w-screen h-screen p-6'>
       <div className='bg-white rounded-md max-w-md p-6 shadow-sm'>
